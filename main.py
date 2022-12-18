1329cf4831437afd2ee97ccd512a631e439e12b5
BozhkoAV <avbozhko@mail.ru>
2022-12-18 12:03:19 +0300

    add amount_check

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import re
 
 def main():
     currency_check(input("Введите конвертируемую валюту (BTC|ETH): "))
+    amount_check(input("Введите количество конвертируемой валюты: "))
 
 
 def currency_check(currency):
@@ -14,6 +15,15 @@ def currency_check(currency):
         return "BTC"
     if re.fullmatch(r"^e|eth$", currency.lower()):
         return "ETH"
+
+
+def amount_check(amount):
+    while not(re.fullmatch(r"^\d+(\.\d+)?", amount)):
+        print("Неверный формат ввода числа\n"
+              "Допустимые варианты ввода: число | целая часть цисла.дробная часть числа\n"
+              "Примеры: 123 | 100.12\n")
+        amount = input("Введите количество конвертируемой валюты: ")
+    return float(amount)
 
 
 if __name__ == '__main__':
